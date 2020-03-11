517032482a7d3b9d5049cd92252ef6d6176d295e
Rodrigo Moreira Carvalho <rodrigo_moreira84@hotmail.com>
2020-03-11 16:56:45 -0300

    alterando a aplicação para o modo transacional de forma explícita

--- src/main/java/com/empresaRest/EmpresaRestApplication.java
+++ src/main/java/com/empresaRest/EmpresaRestApplication.java
@@ -3,9 +3,11 @@ package com.empresaRest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan({ "com.empresaRest" }) 
 @SpringBootApplication
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class EmpresaRestApplication {
 
 	public static void main(String[] args) {
